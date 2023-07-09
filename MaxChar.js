c138592598f7dc401d072912ac4c8b764014e40d
Rajkumar <kuraj10101999@gmail.com>
2023-07-09 18:10:42 +0530

    maximum character in string using obj

---
+++ MaxChar.js
@@ -0,0 +1,25 @@
+
+let str="ASCFCDFCCESC";
+
+let obj={}
+
+for(let i=0; i<str.length; i++)
+{
+    if(obj[str[i]]==undefined)
+    {
+      obj[str[i]] = 1
+    }
+    else{
+        obj[str[i]]++
+    }
+}
+
+let count=0;
+for(k in obj)
+{
+    if(obj[k] >count)
+    {
+        count=obj[k]
+    }
+}
+console.log("str", count)
